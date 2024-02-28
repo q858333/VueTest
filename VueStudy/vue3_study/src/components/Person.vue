7009b1e4504fee4bee6aff207f0d7287f5b0a8c5
dengbin <dengcheez@gmail.com>
2024-02-28 17:51:06 +0800

    ref定义对象类型响应式数据，底层使用的是reactive

--- VueStudy/vue3_study/src/components/Person.vue
+++ VueStudy/vue3_study/src/components/Person.vue
@@ -8,19 +8,38 @@
         <hr>
         <h2>一辆{{car.name}},价值{{car.price}}万 </h2>
         <button @click="changePrice">修改价格</button>
+        <hr>
+        <h2>游戏猎豹</h2>
+        <ul>
+            <li v-for="g in games"  :key="g.id">{{g.name}}</li>
+        </ul>
+        <button @click="changeFirstGame">修改第一个游戏名称</button>
+
 
     </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue' //ref创建-基本类型的响应式数据
-import { reactive } from 'vue';//reactive创建-对象类型的响应式数据
+//ref创建-可以定义基本类型和对象类型的响应式数据
+//reactive创建-只能定义对象类型的响应式数据
+import {ref,reactive} from 'vue' 
 let name = ref('章三');
 let age = ref(18);
 let tel = '123123123123122312';
 
 let car = reactive({name:'奔驰',price:100});
+let games = ref(
+    [
+        {id:'1',name:'天下'},
+        {id:'2',name:'王者'},
+        {id:'3',name:'Dota2'},
 
+    ]
+);
+
+function changeFirstGame () {
+    games.value[0].name = "天下手游"
+}
 function changePrice () {
     car.price+=10;
 }
